23e619f82e41e00ce6ae2b6fd2f4a79848754e64
Reganvalkyrie <regangirikaruna@gmail.com>
2022-04-26 00:28:59 +0700

    feat: change icon

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -8,23 +8,9 @@ export default function Layout({ children }) {
       <Head>
         <title>STORYOFHAPPINESS</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo180x180.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <motion.div
